a7958e7b214b99f5708993e709d8fab3ee1ff291
“Shaoan <swang387@ucsc.edu>
2024-01-19 15:16:23 -0800

    test

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Code Practice: Making a Scene
+// Name: Shaoan Wang
+// Date: 1/17/24
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play ]
+
+}
+
+let game = new Phaser.Game(config)
